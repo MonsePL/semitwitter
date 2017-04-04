1742e8767596603d6c015fe4695fe5281979b187
Monserrat Pineda <monserrat.pineda.luna@gmail.com>
2017-04-04 08:57:59 -0500

    primera version

---
+++ twiiter.js
@@ -0,0 +1,17 @@
+alert("miau")
+
+//no se hacer esto, no funciona :(
+
+var mandarTweet=document.getElementById("bmandartweet")
+mandartweet.addEventListener("click", mandarTweet)
+
+
+function textoabajito() {
+  var texto=document.getElementById('cajitadetweet').value;
+  document.getElementById("textoenvivo").innerHTML = texto
+}
+
+function mandarTweet() {
+  var textot = document.getElementById("nuevoescrito").innerHTML;
+  document.getElementById("textoenvivo").innerHTML = vartexto2;
+}
